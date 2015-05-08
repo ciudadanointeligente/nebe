6b0535dca4464c76e4b53e500b913884dcf21048
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-05-08 13:16:17 -0300

    small changes

--- js/main.js
+++ js/main.js
@@ -1,47 +1,42 @@
 (function ($) {
     "use strict";
 
-    ///////////////////////////////////////////////////// Your
-    var venueAddress = "Liber Bernardo O'Higgins 227, Santiago"; // Venue
-    var placeName = "Centro Gabriela Mistral";
-    /////////////////////////////////////////////////// Adress
-
     var fn = {
 
                 // Launch Functions
-            Launch: function () {
-                    fn.MenuSticky();
-                    fn.MainSliderAlign();
-                    fn.MainSlider();
-                    fn.Slider();
-                },
+        Launch: function () {
+                fn.MenuSticky();
+                fn.MainSliderAlign();
+                fn.MainSlider();
+                fn.Slider();
+            },
 
-                // Sticky Menu
-            MenuSticky: function () {
-                var menu = document.querySelector('#menu'),
-                    origOffsetY = menu.offsetTop + 100;
-                function scroll() {
-                    if ($(window).scrollTop() >= origOffsetY) {
-                        $('#menu').addClass('sticky');
-                        $('#menu').removeClass('fixed');
-                    } else {
-                        $('#menu').removeClass('sticky');
-                        $('#menu').addClass('fixed');
-                    }
+            // Sticky Menu
+        MenuSticky: function () {
+            var menu = document.querySelector('#menu'),
+                origOffsetY = menu.offsetTop + 100;
+            function scroll() {
+                if ($(window).scrollTop() >= origOffsetY) {
+                    $('#menu').addClass('sticky');
+                    $('#menu').removeClass('fixed');
+                } else {
+                    $('#menu').removeClass('sticky');
+                    $('#menu').addClass('fixed');
                 }
-                document.onscroll = scroll;
-            },
+            }
+            document.onscroll = scroll;
+        },
 
-                // One Page Navigation
-            Navigation: function () {
-                $('#menu').onePageNav({
-                    currentClass: 'current',
-                    scrollSpeed: 500,
-                    scrollOffset: 60,
-                    scrollThreshold: 0.2,
-                    easing: 'swing'
-                });
-            },
+            // One Page Navigation
+        Navigation: function () {
+            $('#menu').onePageNav({
+                currentClass: 'current',
+                scrollSpeed: 500,
+                scrollOffset: 60,
+                scrollThreshold: 0.2,
+                easing: 'swing'
+            });
+        },
 
         // Align Slider Horizontally
         MainSliderAlign: function () {
