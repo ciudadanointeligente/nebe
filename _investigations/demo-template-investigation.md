fad3040aab86394c24cbc2131344ab86487c268c
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-05-12 17:18:22 -0300

    add investigations views
    add json of investigations

--- _investigations/demo-template-investigation.md
+++ _investigations/demo-template-investigation.md
@@ -1,4 +1,5 @@
 ---
+layout: investigation
 published: true
 title: "SITUACION DEL CONFLICTO PETROLERO EN EL NORTE DE LA PAZ - BLOQUE LIQUIMUNI"
 description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
--- js/controllers.js
+++ js/controllers.js
@@ -14,4 +14,15 @@ arkakApp.controller("ReportsCtrl", function($scope, $http){
 	 $scope.format_date = function(the_date){
 	 	return new Date(the_date).getTime();
 	 }
-})
+
+});
+
+arkakApp.controller("InvestigationsCtrl", function($scope, $http){
+	$scope.investigations = [];
+
+	$http.get('/data/investigations/investigations.json')
+		.success(function(data){
+			$scope.investigations = data.investigations;
+		})
+
+});
